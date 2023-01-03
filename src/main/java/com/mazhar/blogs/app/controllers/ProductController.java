1dd90e7ce86c736fbd77ec620b11b7488663a6f3
Mazhar <MazharuddinF@winjit.com>
2023-01-03 13:59:37 +0530

    written comment in Dev branch

--- src/main/java/com/mazhar/blogs/app/controllers/ProductController.java
+++ src/main/java/com/mazhar/blogs/app/controllers/ProductController.java
@@ -4,4 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+
+    //this code is for testing purpose and is in main dev branch
 }
